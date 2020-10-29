6b153dfa6bbbfebf23025daf9e36c31a1d9b4868
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2020-10-28 21:54:34 -0300

    novo comit

--- xadrez/src/model/Bispo.java
+++ xadrez/src/model/Bispo.java
@@ -15,7 +15,7 @@ public class Bispo extends Peca{
 		movCount = percorre(x, y, -1, 1, tab, movimentos, movCount);
 		movCount = percorre(x, y, -1, -1, tab, movimentos, movCount);
 		movCount = percorre(x, y, 1, -1, tab, movimentos, movCount);
-		// minha pika
+		// oiiee
 		
 		System.out.println(movCount);
 		
