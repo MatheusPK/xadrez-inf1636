f80a636b24de23dc918a7e8a98ec3b53d00edb81
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-12-13 10:19:54 -0300

    Revert "conserta roque"
    This reverts commit b58b0648ccd00a05c530e877950c966ffce22efb.

--- xadrez/src/controller/ControllerFacade.java
+++ xadrez/src/controller/ControllerFacade.java
@@ -113,7 +113,7 @@ public class ControllerFacade implements Observable{
 	private void verificaMovDisp(int xPeca, int yPeca) {
 		
 		if (ModelFacade.isOutOfBounds(xPeca, yPeca)){
-			//System.out.println("Clique invï¿½lido");
+			//System.out.println("Clique inválido");
 			return;
 		}
 		
@@ -127,7 +127,7 @@ public class ControllerFacade implements Observable{
 		
 		
 		movDisp = ModelFacade.verificaRoqueCurto(xPeca, yPeca, defineVez(), movDisp);
-		movDisp = ModelFacade.verificaRoqueLongo(xPeca, yPeca, defineVez(), movDisp);
+//		movDisp = ModelFacade.verificaRoqueLongo(xPeca, yPeca, defineVez(), movDisp);
 	    
 	    
 	    if (movDisp == null){
@@ -150,7 +150,7 @@ public class ControllerFacade implements Observable{
 			return;
 		}
 		if (ModelFacade.isOutOfBounds(xPeca, yPeca) || !ModelFacade.isPosInMov(movDisp, xPeca, yPeca)){
-			//System.out.printf("Movimento Invï¿½lido!\n");
+			//System.out.printf("Movimento Inválido!\n");
 			return;
 		} 
 				
