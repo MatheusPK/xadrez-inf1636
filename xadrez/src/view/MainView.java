2452eb6f1b3e59c14e9c5dc4c426b301d97befdc
TheoCaldas <62903584+TheoCaldas@users.noreply.github.com>
2020-12-13 15:50:02 -0300

    tela inicial

--- xadrez/src/view/MainView.java
+++ xadrez/src/view/MainView.java
@@ -1,17 +1,51 @@
 package view;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MainView extends JFrame{
 
     public MainView(TabuleiroView tv, int DEFAULT_HEIGHT, int DEFAULT_WIDTH) {
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Dimension screenSize=tk.getScreenSize();
-		int screenWidth = screenSize.width;
-		int screenHeight = screenSize.height;
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize=tk.getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
         setBounds(screenWidth/2 - DEFAULT_WIDTH/2, screenHeight/2 - DEFAULT_HEIGHT/2, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        getContentPane().add(tv);
+        
+        JPanel f=new JPanel();  
+        f.setSize(screenSize);
+        JButton novoJogo = new JButton("Novo Jogo");
+        JButton carregarJogo = new JButton("Carregar Jogo"); 
+        
+        novoJogo.setBounds(325, 340, 150, 30);
+        f.add(novoJogo);  
+        
+        carregarJogo.setBounds(325,400,150,30);  
+        f.add(carregarJogo);
+        
+        novoJogo.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){ 
+                System.out.println("oi");
+                getContentPane().remove(f);
+                repaint(); //provavelmente causa o bug do double click
+                getContentPane().add(tv); 
+            }  
+        });  
+        
+        carregarJogo.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                
+            }  
+        }); 
+        f.setSize(400,400);  
+        f.setLayout(null);
+        f.setVisible(true);
+        add(f);
+        //getContentPane().add(f);
+        //getContentPane().add(tv);
     }
     
     
